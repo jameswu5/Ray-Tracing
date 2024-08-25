36e5e1cf344b3d3507791d0629bdacce4ca1530e
James Wu <jameswu144@gmail.com>
2024-08-25 22:23:54 +0100

    add color class

---
+++ color.h
@@ -0,0 +1,21 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color pixel_color) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Write the translated [0, 255] value of each color component
+    out << int(255.999 * r) << ' '
+        << int(255.999 * g) << ' '
+        << int(255.999 * b) << '\n';
+}
+
+#endif
