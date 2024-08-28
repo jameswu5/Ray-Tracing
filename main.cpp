b03ae28f1ac8a37589bcaae0d332f6e40eb888a0
James Wu <jameswu144@gmail.com>
2024-08-28 12:36:17 +0100

    optimise code

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 
 double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b*b - 4*a*c;
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = h * h - a * c;
 
-    return discriminant < 0 ? -1.0 : (-b - std::sqrt(discriminant)) / (2.0 * a);
+    return discriminant < 0 ? -1.0 : (h - std::sqrt(discriminant)) / a;
 }
 
 color ray_color(const ray& r) {
