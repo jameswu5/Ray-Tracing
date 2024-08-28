9f511acd239d1abfd8804a116521b9d22199b0f0
James Wu <jameswu144@gmail.com>
2024-08-28 12:13:54 +0100

    fix bugs

--- vec3.h
+++ vec3.h
@@ -8,8 +8,12 @@ class vec3 {
     public:
         double e[3];
 
-        vec3() : e{0, 0, 0} {}
-        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+        vec3() { e[0] = 0; e[1] = 0; e[2] = 0; }
+        vec3(double e0, double e1, double e2) {
+            e[0] = e0;
+            e[1] = e1;
+            e[2] = e2;
+        }
 
         double x() const { return e[0]; }
         double y() const { return e[1]; }
@@ -46,11 +50,13 @@ class vec3 {
         }
 };
 
+// point3 is just an alias for vec3, but useful for geometric clarity in the code.
 using point3 = vec3;
+
 
 // Vector Utility Functions
 
-inline std::ostream& operator <<(std::ostream& out, const vec3& v) {
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
@@ -74,7 +80,7 @@ inline vec3 operator*(const vec3& v, double t) {
     return t * v;
 }
 
-inline vec3 operator/(vec3 v, double t) {
+inline vec3 operator/(const vec3& v, double t) {
     return (1/t) * v;
 }
 
@@ -90,7 +96,7 @@ inline vec3 cross(const vec3& u, const vec3& v) {
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector(vec3 v) {
+inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
 }
 
