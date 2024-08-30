68df87b1709acf4668b24a46fc51f093e25d1557
James Wu <jameswu144@gmail.com>
2024-08-30 21:23:26 +0100

    fix bug

--- camera.h
+++ camera.h
@@ -79,6 +79,10 @@ class camera {
         }
 
         color ray_color(const ray& r, int depth, const hittable& world) const {
+            if (depth <= 0) {
+                return color(0, 0, 0);
+            }
+
             hit_record rec;
 
             if (world.hit(r, interval(0.001, infinity), rec)) {
